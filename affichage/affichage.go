28f6ca7cea5cbb667ec56c2f11eb377d88fd4a5f
Emrick <emrick.rivet@ynov.com>
2025-10-21 12:23:08 +0200

    Début des menu

--- affichage/affichage.go
+++ affichage/affichage.go
@@ -6,12 +6,33 @@ func Separator() {
 	fmt.Println("==================================================")
 }
 
-func AffichageMenu() {
+func MenuAccueil() {
 	Separator()
 	fmt.Println("Bienvenue au Quiz Go")
 	Separator()
 	fmt.Println("1 - ▶️  Commencer une nouvelle partie")
 	fmt.Println("2 - 🪺  On verra plus tard")
-	fmt.Println("3 - 👋 Quitter")
+	fmt.Println("3 - 👋 Quitter le jeu")
 	Separator()
+}
+
+func MenuPrincipale() {
+	Separator()
+	fmt.Println("Choix principal")
+	Separator()
+	fmt.Println("1 - 🆕 Nouveaux quizz")
+	fmt.Println("2 - 📚 Statistiques")
+	fmt.Println("3 - 👋 Retour à l'accueil")
+	Separator()
+}
+func MenuQuizz() {
+	Separator()
+	fmt.Println("Choix du quizz")
+	Separator()
+	fmt.Println("1 - 🖥️  Quizz Informatique")
+	fmt.Println("2 - 🛡️  Quizz cyber sécurité")
+	fmt.Println("3 - 🗄️  Quizz Data")
+	fmt.Println("4 - 👋 Retour")
+	Separator()
+
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Quizz-Go/affichage"
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -12,19 +13,69 @@ func main() {
 
 	// Boucle principale du jeu
 	for {
+		menuChoice = 0
 		// Effacer l'écran
 		fmt.Print("\033[H\033[2J")
 
 		// Affichage du menu de démarrage
-		affichage.AffichageMenu()
+		affichage.MenuAccueil()
 		fmt.Scan(&menuChoice)
 		switch menuChoice {
 		case 1:
-			fmt.Println("On verra ...")
+			for {
+				menuChoice = 0
+				fmt.Print("\033[H\033[2J")
 
+				affichage.MenuPrincipale()
+				fmt.Scan(&menuChoice)
+				switch menuChoice {
+				case 1:
+					fmt.Println("Nouveaux quizz")
+					for {
+						menuChoice = 0
+						fmt.Print("\033[H\033[2J")
+
+						affichage.MenuQuizz()
+						fmt.Scan(&menuChoice)
+						switch menuChoice {
+						case 1:
+							fmt.Println("Quizz Informatique")
+							// Appeler la fonction du quizz info
+						case 2:
+							fmt.Println("Quizz cyber sécurité")
+							// Appeler la fonction du quizz cyber sécurité
+						case 3:
+							fmt.Println("Quizz Data")
+							// Appeler la fonction du quizz data
+						case 4:
+							menuChoice = 4
+						default:
+							fmt.Println("Choix invalide, veuillez réessayer.")
+						}
+
+						if menuChoice == 4 {
+							break
+						}
+					}
+				case 2:
+					fmt.Println("Statistiques")
+					// Faire une fonction qui montre le nom + score des quizz déjà fait + le score total
+				case 3:
+					menuChoice = 3
+				default:
+					fmt.Println("Choix invalide, veuillez réessayer.")
+				}
+				if menuChoice == 3 {
+					break
+				}
+			}
 		case 2:
 			fmt.Println("On verra ...")
+		case 3:
+			fmt.Println("Adios cheerios !")
+			os.Exit(0)
+		default:
+			fmt.Println("Choix invalide, veuillez réessayer.")
 		}
-
 	}
 }
