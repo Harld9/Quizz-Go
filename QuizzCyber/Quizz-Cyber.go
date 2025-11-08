f37d72607291b7a034a09fffe2f27be138e01689
Harold <harold.francois@ynov.com>
2025-11-08 12:55:10 +0100

    Correction bug main.go et Quizz-Cyber.go

--- QuizzCyber/Quizz-Cyber.go
+++ QuizzCyber/Quizz-Cyber.go
@@ -1,71 +1,58 @@
 package cyber
 
-import (
-	"fmt"
-)
+/*var menuChoice int
 
-func Cyber() {
-	var menuChoice int
-	affichage.AffichageMenuCyber()
-	fmt.Scan(&menuChoice)
-	switch menuChoice {
-
-	}
-
-	/*var menuChoice int
-
-		for {
+	for {
+		fmt.Print("\033[H\033[2J")
+		affichage.AffichageMenuCyber()
+		fmt.Scan(&menuChoice)
+		switch menuChoice {
+		case 1:
 			fmt.Print("\033[H\033[2J")
-			affichage.AffichageMenuCyber()
-			fmt.Scan(&menuChoice)
-			switch menuChoice {
-			case 1:
-				fmt.Print("\033[H\033[2J")
-
-			}
-			affichage.AffichageMenuPrincipal()
-			fmt.Scan(&menuChoice)
-
-			switch menuChoice {
-			case 1:
-
-				// Effacer l'écran
-				fmt.Print("\033[H\033[2J")
-
-			case 2:
-
-				// Effacer l'écran
-				fmt.Print("\033[H\033[2J")
-
-			case 3:
-
-				// Effacer l'écran
-				fmt.Print("\033[H\033[2J")
-
-			case 4:
-
-				// Effacer l'écran
-				fmt.Print("\033[H\033[2J")
-
-			case 5:
-
-				// Effacer l'écran
-				fmt.Print("\033[H\033[2J")
-
-			case 6:
-
-			default:
-				fmt.Print("\033[H\033[2J")
-				fmt.Printf("\n❌ Il ne se passe rien... Choix invalide.\n")
-			}
-
-			// Reset de la variable menuChoice pour éviter les boucles infinies
-			if menuChoice == 6 {
-				menuChoice = 0
-				// Retour au menu précédent (menu de démarrage)
-				break
-			}
 
 		}
-	}*/
-}
+		affichage.AffichageMenuPrincipal()
+		fmt.Scan(&menuChoice)
+
+		switch menuChoice {
+		case 1:
+
+			// Effacer l'écran
+			fmt.Print("\033[H\033[2J")
+
+		case 2:
+
+			// Effacer l'écran
+			fmt.Print("\033[H\033[2J")
+
+		case 3:
+
+			// Effacer l'écran
+			fmt.Print("\033[H\033[2J")
+
+		case 4:
+
+			// Effacer l'écran
+			fmt.Print("\033[H\033[2J")
+
+		case 5:
+
+			// Effacer l'écran
+			fmt.Print("\033[H\033[2J")
+
+		case 6:
+
+		default:
+			fmt.Print("\033[H\033[2J")
+			fmt.Printf("\n❌ Il ne se passe rien... Choix invalide.\n")
+		}
+
+		// Reset de la variable menuChoice pour éviter les boucles infinies
+		if menuChoice == 6 {
+			menuChoice = 0
+			// Retour au menu précédent (menu de démarrage)
+			break
+		}
+
+	}
+}*/
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	cyber "Quizz-Go/Quizz"
 	"Quizz-Go/affichage"
 	"Quizz-Go/logic"
 	"fmt"
@@ -12,10 +11,7 @@ func main() {
 
 	// Initialisation du choix du menu
 	var menuChoice int
-<<<<<<< HEAD
 	affichage.ClearScreen()
-=======
->>>>>>> cyber
 	// Boucle principale du jeu
 	for {
 		menuChoice = 0
@@ -78,7 +74,6 @@ func main() {
 		case 2:
 			affichage.ClearScreen()
 			fmt.Println("On verra ...")
-<<<<<<< HEAD
 			fmt.Println("Florian est un gros nul en Go 😂 et Harold adore sa copine avec une baguette de 7cm attaché sur elle *Miam*")
 		case 3:
 			affichage.ClearScreen()
@@ -87,12 +82,6 @@ func main() {
 		default:
 			affichage.ClearScreen()
 			fmt.Println("Choix invalide, veuillez réessayer.")
-=======
-
-		case 3:
-			cyber.Quizzcyber()
-
->>>>>>> cyber
 		}
 	}
 }
