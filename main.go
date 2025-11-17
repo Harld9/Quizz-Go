7b6d32ad4b90a84e005325719aa84557140380ba
Emrick <emrick.rivet@ynov.com>
2025-11-17 12:47:22 +0100

    Modif message deuxeme choix accueil

--- main.go
+++ main.go
@@ -76,8 +76,7 @@ func main() {
 			}
 		case 2:
 			affichage.ClearScreen()
-			fmt.Println("On verra ...")
-			fmt.Println("Florian est un gros nul en Go 😂 et Harold adore sa copine avec une baguette de 7cm attaché sur elle *Miam*")
+			fmt.Println("Bienvenu ! Voici un jeu de quizz dans les thèmes de l'informatique/dévelopement, de la cyber sécurité, et de la data !\n Il y a 10 questions par quizz et tu pourras voir tes statistique dans un menu !")
 		case 3:
 			affichage.ClearScreen()
 			fmt.Println("Adios cheerios !")
